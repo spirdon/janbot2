c580f4760f4429dd33fb8fbaa9ec48b2942cd1e5
spirdon <ununtrium1337@gmail.com>
2018-10-21 23:40:53 +0200

    Added SzkalujeCommand

--- janbot2.py
+++ janbot2.py
@@ -13,6 +13,7 @@ import jb2.misc.ankieta
 import jb2.text.ask
 import jb2.text.elo
 import jb2.text.przondlo
+import jb2.text.szkaluje
 #import jb2.video.comm_cenzovid # doesn't work well
 
 
@@ -27,7 +28,8 @@ commands = (
     jb2.misc.ankieta.AnkietaCommand(),
     jb2.text.ask.AskCommand(),
     jb2.text.elo.EloCommand(),
-    jb2.text.przondlo.PrzondloCommand()
+    jb2.text.przondlo.PrzondloCommand(),
+    jb2.text.szkaluje.SzkalujeCommand()
 )
 
 
---
+++ jb2/text/szkaluje.py
@@ -0,0 +1,34 @@
+import random
+import re
+import discord
+
+import jb2.command
+import jb2.embed
+
+
+class SzkalujeCommand(jb2.command.Command):
+    def __init__(self):
+        with open('res/text/szkalunki.txt') as file:
+            self.slanders = file.readlines()
+
+    def get_pattern(self):
+        return r'szkaluj( .+)?$'
+
+    async def action(self, connector, message, client):
+        msg = message.content.strip()
+        author_m = message.author.mention
+        pattern = self.get_pattern()
+
+        slandered = re.match("^" + pattern, msg).group(1)
+
+        if slandered in [None, '']:
+            text = "Potrzebny parametr: **nick**"
+            emb = jb2.embed.error_embed(author_m, text)
+        else:
+            footer_text = "Szkalunek dla " + str(message.author)
+
+            emb = discord.Embed()
+            emb.description = random.choice(self.slanders).format(slandered)
+            emb.set_footer(text=footer_text)
+
+        await client.send_message(message.channel, embed=emb)
